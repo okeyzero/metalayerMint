620e207439517efa8e512cbb1a6d3a548804850b
okeyzero <48344256+okeyzero@users.noreply.github.com>
2024-07-25 02:24:55 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,183 @@
+import axios from "axios";
+import {wrapper} from 'axios-cookiejar-support';
+import {CookieJar} from 'tough-cookie';
+import fs from "fs";
+import pLimit from 'p-limit';
+import {ethers} from "ethers";
+import Logger from "@youpaichris/logger";
+import path from "path";
+import {fileURLToPath} from 'url';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url)
+const logger = new Logger(path.basename(__filename));
+const successPath = "success.txt";
+const failPath = "fail.txt";
+
+const UserToken = process.env.USERTOKEN;
+const thread = parseInt(process.env.THREAD) || 1;
+const DeveloperId = "hLf08E"
+
+const allProxy = fs.readFileSync("proxys.txt", "utf-8").split("\n")
+logger.info(`代理总数 ${allProxy.length}`)
+
+
+async function mint(client, address, signature, turnstileToken) {
+    const response = await client.post(
+        'https://metalayer.caldera.xyz/api/trpc/mint.mint',
+        {
+            '0': {
+                'json': {
+                    'address': address,
+                    'signature': signature,
+                    'turnstileToken': turnstileToken
+                }
+            }
+        },
+        {
+            params: {
+                'batch': '1'
+            },
+            headers: {
+                'Accept': '*/*',
+                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+                'Cache-Control': 'no-cache',
+                'Connection': 'keep-alive',
+                'Origin': 'https://metalayer.caldera.xyz',
+                'Pragma': 'no-cache',
+                'Referer': 'https://metalayer.caldera.xyz/mint',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+                'content-type': 'application/json',
+                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'trpc-accept': 'application/jsonl',
+                'x-trpc-source': 'nextjs-react'
+            }
+        }
+    );
+    return response.data;
+}
+
+async function get_request_token() {
+    const proxy = allProxy[Math.floor(Math.random() * allProxy.length)]
+    const url = "http://api.nocaptcha.io/api/wanda/cloudflare/universal"
+    const data = {
+        "href": "https://metalayer.caldera.xyz/mint",
+        "proxy": proxy,
+        "sitekey": "0x4AAAAAAAft2frgHoEZW9zt",
+        "explicit": true
+    }
+    const response = await axios.post(
+        url,
+        data,
+        {
+            headers: {
+                'User-Token': UserToken,
+                'Content-Type': 'application/json',
+                'Developer-Id': DeveloperId,
+            }
+        }
+    );
+    // console.log(response.data)
+    return response.data;
+
+}
+
+
+async function mintNft(privateKey) {
+    const jar = new CookieJar();
+    const client = wrapper(axios.create({jar}));
+    const wallet = new ethers.Wallet(privateKey);
+    const myAddress = wallet.address;
+
+    const message = "I, ".concat(myAddress, ", am ready to enter the Caldera Metalayer.")
+    const signature = await wallet.signMessage(message);
+
+    let requestToken;
+    for (let i = 0; i < 10; i++) {
+        try {
+            logger.debug(`${myAddress} get request token... `)
+            requestToken = (await get_request_token())?.data?.token
+            if (requestToken) {
+                break
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    if (!requestToken) {
+        logger.error(`${myAddress} get request token failed`)
+        fs.appendFileSync(
+            failPath,
+            `${myAddress}----${privateKey}\n`
+        );
+        return
+    }
+
+    let toAddress, toAmount;
+    for (let i = 0; i < 5; i++) {
+        try {
+            const request = await mint(client, myAddress, signature, requestToken)
+            // console.log(JSON.stringify(request, null, 2))
+            logger.info(`${myAddress}  ${JSON.stringify(request, null, 2)}`)
+            if (request) {
+                break
+            }
+        } catch (e) {
+            // console.error(`Error Status: ${e?.response?.status}`);
+            // console.error('Error Response:', JSON.stringify(e?.response?.data));
+
+            logger.error(`${myAddress} Error Status:  ${e?.response?.status} Error Response: ${JSON.stringify(e?.response?.data)}`)
+            //如果 max mints 在 e?.response?.data 的内容里 失败
+            if (JSON.stringify(e?.response?.data).includes("error")) {
+                fs.appendFileSync(
+                    failPath,
+                    `${myAddress}----${privateKey}\n`
+                );
+                return
+            }
+
+        }
+    }
+    fs.appendFileSync(
+        successPath,
+        `${myAddress}----${privateKey}\n`
+    );
+}
+
+async function main() {
+    //读取keys.txt
+    const keys = fs
+        .readFileSync('keys.txt', "utf8")
+        .split(/\r?\n/)
+        .filter((key) => key);
+
+    const successAddress = fs
+        .readFileSync(successPath, "utf8")
+        .split(/\r?\n/)
+        .filter((key) => key)
+        .map((key) => key.split("----")[0].toLocaleLowerCase());
+
+    const limit = pLimit(thread);
+    const tasks = keys.map((key) => {
+        const [address, privateKey] = key.split('----');
+        if (successAddress.includes(address.toLocaleLowerCase())) {
+            logger.info(`${address} 已经领取过了`);
+            return Promise.resolve();
+        }
+        return limit(() => mintNft(privateKey));
+    });
+
+    await Promise.all(tasks);
+
+    logger.success("done");
+}
+
+main().catch(
+    (e) => console.error(e)
+)
